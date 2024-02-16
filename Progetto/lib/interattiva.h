f004f6b179f4ffdcd7fe3438e7568f577a2d4145
chxxxxto <158204794+chxxxxto@users.noreply.github.com>
2024-02-16 18:05:32 +0100

    Add files via upload

--- Progetto/lib/interattiva.h
+++ Progetto/lib/interattiva.h
@@ -5,5 +5,6 @@
 
 extern int menu_scelta();
 extern void stampa_mano(Lista_doppia* ptr);
+extern void tabellone(Nodo* ptr, Lista_doppia* a, int left);
 
 #endif //PROGETTO_INTERATTIVA_H
--- Progetto/lib/lista.h
+++ Progetto/lib/lista.h
@@ -22,5 +22,8 @@ extern void free_lista(Lista_doppia* ptr);
 extern Nodo* pop_casuale(Lista_doppia* ptr, int i);
 extern void crea_lista(int n, Lista_doppia* a);
 extern int num_elementi(Lista_doppia* ptr);
+extern void inizializza_valore(Nodo* ptr, Lista_doppia* a);
+extern void push_front_valore(Nodo* ptr, Lista_doppia* a);
+extern void push_back_valore(Nodo* ptr, Lista_doppia* a);
 
 #endif
