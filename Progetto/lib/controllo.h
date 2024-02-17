c015a4f4588785868237107d478a6c461532e4a6
chxxxxto <158204794+chxxxxto@users.noreply.github.com>
2024-02-17 12:45:24 +0100

    Add files via upload

---
+++ Progetto/lib/controllo.h
@@ -0,0 +1,7 @@
+#ifndef PROGETTO_CONTROLLO_H
+#define PROGETTO_CONTROLLO_H
+
+extern void puntatore_nullo_memory(void* ptr);
+extern void puntatore_nullo_data(void* a, void* b);
+
+#endif //PROGETTO_CONTROLLO_H
