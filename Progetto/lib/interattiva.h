3b58ee89a1b8f1cb4aabbc1808afe076c84653c1
STMarcoStevanato <131982944+STMarcoStevanato@users.noreply.github.com>
2024-02-17 17:56:17 +0100

    Add files via upload

--- Progetto/lib/interattiva.h
+++ Progetto/lib/interattiva.h
@@ -6,6 +6,9 @@
 extern int menu_scelta();
 extern int scelta_tessera(Lista_doppia* ptr);
 extern void gira_tessera(Lista_doppia* ptr, int i);
+extern void tessera_campo(Lista_doppia* mano, Lista_doppia* tavolo, int i);
 extern void tabellone(Nodo* ptr, Lista_doppia* a, int left);
+extern int controllo_dx(Nodo* a, Lista_doppia* tavolo);
+extern int controllo_lx(Nodo* a, Lista_doppia* tavolo);
 
 #endif //PROGETTO_INTERATTIVA_H
--- Progetto/lib/lista.h
+++ Progetto/lib/lista.h
@@ -22,6 +22,7 @@ extern void push_front_valore(Nodo* ptr, Lista_doppia* a);
 extern void push_back_valore(Nodo* ptr, Lista_doppia* a);
 extern Nodo* pop_indice(Lista_doppia* ptr, int i);
 extern void crea_lista(int n, Lista_doppia* a);
+extern void crea_lista_vuota(Lista_doppia* a);
 extern void stampa_lista(Lista_doppia* ptr);
 extern void stampa_lista_numerata(Lista_doppia* ptr);
 extern int num_elementi(Lista_doppia* ptr);
