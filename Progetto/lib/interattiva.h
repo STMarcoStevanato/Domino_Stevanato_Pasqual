f36416edf792f53e86965a7c7756e6dd9b3bc6e3
chxxxxto <158204794+chxxxxto@users.noreply.github.com>
2024-02-18 23:51:16 +0100

    Add files via upload

--- Progetto/lib/interattiva.h
+++ Progetto/lib/interattiva.h
@@ -8,7 +8,7 @@ extern int scelta_tessera(Lista_doppia* ptr);
 extern void gira_tessera(Lista_doppia* ptr, int i);
 extern void tessera_campo(Lista_doppia* mano, Lista_doppia* tavolo, int i);
 extern void tabellone(Nodo* ptr, Lista_doppia* a, int left);
-extern int controllo_dx(Nodo* a, Lista_doppia* tavolo);
-extern int controllo_lx(Nodo* a, Lista_doppia* tavolo);
+extern int controllo_dx(Nodo* ptr, Lista_doppia* tavolo);
+extern int controllo_sx(Nodo* ptr, Lista_doppia* tavolo);
 
 #endif //PROGETTO_INTERATTIVA_H
