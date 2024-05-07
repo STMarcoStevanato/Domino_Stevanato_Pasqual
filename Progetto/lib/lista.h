6fc5f6d9a3973a58be3016a35ba13b104f3f39e4
chxxxxto <158204794+chxxxxto@users.noreply.github.com>
2024-05-07 10:18:01 +0200

    Add files via upload

--- Progetto/lib/lista.h
+++ Progetto/lib/lista.h
@@ -20,7 +20,7 @@ void push_front_random(Lista_doppia* ptr);
 void push_back_random(Lista_doppia* ptr);
 void push_front_valore(Nodo* ptr, Lista_doppia* a);
 void push_back_valore(Nodo* ptr, Lista_doppia* a);
-Nodo* pop_indice(Lista_doppia* ptr, int i);
+void pop_indice(Lista_doppia* ptr, int i);
 void crea_lista(int n, Lista_doppia* a);
 void crea_lista_vuota(Lista_doppia* a);
 void stampa_lista(Lista_doppia* ptr);
