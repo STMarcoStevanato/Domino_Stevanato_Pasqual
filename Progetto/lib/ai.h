db055b7cb104efcbd42beee4364c8868a1137943
chxxxxto <158204794+chxxxxto@users.noreply.github.com>
2024-06-18 00:46:51 +0200

    Add files via upload

--- Progetto/lib/ai.h
+++ Progetto/lib/ai.h
@@ -1,4 +1,7 @@
 #ifndef PROGETTO_AI_H
 #define PROGETTO_AI_H
 
+Lista_doppia* settings(Lista_doppia* mano);
+Lista_doppia* risolvi(Lista_doppia* mano, Lista_doppia* tavolo);
+
 #endif //PROGETTO_AI_H
--- Progetto/lib/partita.h
+++ Progetto/lib/partita.h
@@ -5,7 +5,7 @@ int menu_scelta();
 int controllo_dx(Nodo* ptr, Lista_doppia* tavolo);
 int controllo_sx(Nodo* ptr, Lista_doppia* tavolo);
 void score(Nodo* nuovaTessera);
-int game_over(Lista_doppia* tavolo, Lista_doppia* mano);
+int game_continue(Lista_doppia* tavolo, Lista_doppia* mano);
 int get_score();
 
 #endif //PROGETTO_PARTITA_H
