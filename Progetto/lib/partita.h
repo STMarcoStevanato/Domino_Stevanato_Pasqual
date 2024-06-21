7eb7c94611a0aeaa4491519218618b0a33947ecd
STMarcoStevanato <131982944+STMarcoStevanato@users.noreply.github.com>
2024-06-21 17:24:41 +0200

    Add files via upload

--- Progetto/lib/partita.h
+++ Progetto/lib/partita.h
@@ -2,11 +2,13 @@
 #define PROGETTO_PARTITA_H
 
 int menu_scelta();
+int menu_modalita();
 int controllo_dx(Nodo* ptr, Lista_doppia* tavolo);
 int controllo_sx(Nodo* ptr, Lista_doppia* tavolo);
 void score(Nodo* nuovaTessera);
 void score_ai(Lista_doppia* nuovaTessera);
 int game_continue(Lista_doppia* tavolo, Lista_doppia* mano);
 int get_score();
+void stampa_punteggio();
 
 #endif //PROGETTO_PARTITA_H
