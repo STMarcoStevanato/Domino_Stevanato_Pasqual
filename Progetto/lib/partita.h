d6c5e0a576241ad0257d4be5398d8848b56a10d3
chxxxxto <158204794+chxxxxto@users.noreply.github.com>
2024-06-21 16:19:39 +0200

    Add files via upload

--- Progetto/lib/partita.h
+++ Progetto/lib/partita.h
@@ -5,6 +5,7 @@ int menu_scelta();
 int controllo_dx(Nodo* ptr, Lista_doppia* tavolo);
 int controllo_sx(Nodo* ptr, Lista_doppia* tavolo);
 void score(Nodo* nuovaTessera);
+void score_ai(Lista_doppia* nuovaTessera);
 int game_continue(Lista_doppia* tavolo, Lista_doppia* mano);
 int get_score();
 
