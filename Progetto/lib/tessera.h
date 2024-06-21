f9baaf996a6174cff3d45068bf867bfd336006e5
STMarcoStevanato <131982944+STMarcoStevanato@users.noreply.github.com>
2024-06-21 15:42:22 +0200

    Add files via upload

--- Progetto/lib/tessera.h
+++ Progetto/lib/tessera.h
@@ -10,5 +10,6 @@ void set_tessera(Tessera* tessera, int dx, int sx);
 void set_tessera_random(Tessera* tessera);
 void stampa_tessera(Tessera* tessera);
 void swap(Tessera* tessera);
+int val_tessera(Tessera* tessera);
 
 #endif
