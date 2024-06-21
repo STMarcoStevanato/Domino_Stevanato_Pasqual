9f580913f5835875922d8336d2ad04c8f048a02e
chxxxxto <158204794+chxxxxto@users.noreply.github.com>
2024-06-21 22:45:09 +0200

    Add files via upload

--- Progetto/lib/ai.h
+++ Progetto/lib/ai.h
@@ -3,5 +3,7 @@
 
 Lista_doppia* settings(Lista_doppia* mano);
 Lista_doppia* risolvi(Lista_doppia* mano, Lista_doppia* tavolo);
+Lista_doppia* settare(Lista_doppia* mano);
+Lista_doppia* combinazioni(Lista_doppia* mano, Lista_doppia* risultante, Lista_doppia* salvata);
 
 #endif //PROGETTO_AI_H
